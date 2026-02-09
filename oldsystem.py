887e18c05b8442cbe7f182f9c648dc6cc9ee2bb4
miss-b4teman <ocf2@kent.ac.uk>
2026-02-09 18:02:13 +0000

    rfe

--- oldsystem.py
+++ oldsystem.py
@@ -44,12 +44,12 @@ def run_system_monolith():
         elif opt == "3":
             rem = input("Name to remove: ")
 
-        if rem in n:   
-            idx = n.index(rem)
-            n.pop(idx)
-            r.pop(idx)
-            d.pop(idx)
-            print("Removed.")
+            if rem in n:   
+                idx = n.index(rem)
+                n.pop(idx)
+                r.pop(idx)
+                d.pop(idx)
+                print("Removed.")
             
         elif opt == "4":
             print("Analyzing...")
